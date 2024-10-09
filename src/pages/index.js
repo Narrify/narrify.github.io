f05d93f958d0771e98d1d4c115ee34c22d2bb8ff
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2024-10-09 01:36:01 -0500

    Init

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
